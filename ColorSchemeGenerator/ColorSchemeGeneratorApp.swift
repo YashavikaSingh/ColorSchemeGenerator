012cb21edc67e5a72112197fd4bb39ed5ceaf10f
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-05-27 20:06:28 +0000

    Initial Commit

---
+++ ColorSchemeGenerator/ColorSchemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSchemeGeneratorApp.swift
+//  ColorSchemeGenerator
+//
+//  Created by Yashavika Singh on 27.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSchemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSchemeGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorSchemeGenerator
+//
+//  Created by Yashavika Singh on 27.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
