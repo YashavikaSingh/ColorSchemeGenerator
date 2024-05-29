427a175a3895c6b49c5e3c15b6b5f8bbe4dee9f5
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-05-29 19:01:00 +0000

    Working clipboard functionality

--- ColorSchemeGenerator/ContentView.swift
+++ ColorSchemeGenerator/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     private let pasteboard = UIPasteboard.general
     @StateObject private var array = Scheme()
+    @State private var showingAlert = false
+    @State private var lastCopiedHex: String = ""
+    @State private var showCopiedNotification = false
     var body: some View {
         VStack {
             Text("Color Scheme Generator").font(.custom("Georgia", size: 40))
@@ -24,24 +27,59 @@ struct ContentView: View {
                     }
                 .padding()
             }
-            .padding()
+        .padding()
+        .overlay(
+            copiedNotification()
+                .opacity(showCopiedNotification ? 1 : 0)
+                .animation(.easeInOut(duration: 0.3), value: showCopiedNotification)
+        )
+        
+       
+           
     }
+    
+    
+    func copiedNotification() -> some View {
+            VStack {
+                Spacer()
+                Text("Hex value \(lastCopiedHex) copied to clipboard")
+                    .padding()
+                    .background(Color.black.opacity(0.7))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding(.bottom, 50)
+            }
+        }
     
         func box(particularColor: Color) -> some View
         {
-            
+            let textColor: Color = particularColor.brightness() > 0.5 ? .black : .white
 
-            ZStack
+
+            return ZStack
             {
                 RoundedRectangle(cornerRadius: 25.0)
                     .padding(5)
                     .foregroundStyle(Color(particularColor))
                     .onTapGesture(perform: {
                         pasteboard.string = particularColor.hex()
+                        lastCopiedHex = particularColor.hex()
+                        showCopiedNotification = true
+                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                     showCopiedNotification = false
+                      }
                     })
+                  
 
-                Text(particularColor.hex()).font(.footnote).foregroundStyle(particularColor.brightness() > 0.5 ? .black : .white)
+                Text(particularColor.hex()).font(.footnote).foregroundStyle(textColor)
+
             }
+            
+            
+            .onTapGesture{
+                pasteboard.string = particularColor.hex()
+            }
+                
         }
     
 }
