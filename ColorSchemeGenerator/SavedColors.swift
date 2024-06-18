80d086007111cc11812a0338adfd4297c19aa140
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-18 14:07:56 +0000

    added saved colors view

---
+++ ColorSchemeGenerator/SavedColors.swift
@@ -0,0 +1,18 @@
+//
+//  SavedColors.swift
+//  ColorSchemeGenerator
+//
+//  Created by Yashavika Singh on 18.06.24.
+//
+
+import SwiftUI
+
+struct SavedColors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedColors()
+}
